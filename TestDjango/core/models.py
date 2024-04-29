dd6064baf0c982227a395148d14503ce52375e2c
alejandro <al.gatino@duouc.cl>
2024-04-29 16:27:28 -0400

    actualizar 16:27

--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='id')
     nombre = models.CharField(max_length=100)
     edad = models.IntegerField()
     correo = models.EmailField()
